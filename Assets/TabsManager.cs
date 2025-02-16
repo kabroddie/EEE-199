7fdb6415ef018679293c8d3af68fbf1c976206ad
yugotrope <jumalonraj55@gmail.com>
2025-02-16 16:39:09 +0800

    Navigation Bar and Tabs
    Working navigation bar, switches to different panels but without animations yet

---
+++ Assets/TabsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabsManager : MonoBehaviour
+{
+    public GameObject[] Tabs;
+    public Button[] TabButtons; // Assign buttons in the inspector
+
+    private Color activeColor = Color.green;
+    private Color inactiveColor = Color.white;
+    private int defaultTab = 0;
+    void Start()
+    {
+        SwitchToTab(defaultTab);
+    }
+
+    public void SwitchToTab(int TabID)
+    {
+        foreach(GameObject go in Tabs)
+        {
+            go.SetActive(false);
+        }
+        Tabs[TabID].SetActive(true);
+
+        foreach(Button boon in TabButtons)
+        {
+            Image buttonImage = boon.GetComponent<Image>();
+            buttonImage.color = inactiveColor;
+        }
+        Image buttonActive = TabButtons[TabID].GetComponent<Image>();
+        buttonActive.color = activeColor;
+    }
+}
+
