b023672bf7f79882f54daa32e1cb82466d2508b0
yugotrope <jumalonraj55@gmail.com>
2025-05-13 09:57:48 +0800

    Removed DebugText

--- Assets/SettingsEnabler.cs
+++ Assets/SettingsEnabler.cs
@@ -6,19 +6,16 @@ public class SettingsEnabler : MonoBehaviour
 {
     public GameObject settings;
     public GameObject reminderPanel;
-    public GameObject debugText;
 
     private void Start()
     {
         reminderPanel.SetActive(true);
-        debugText.SetActive(false);
         
     }
 
     public void ScanFirst()
     {
         reminderPanel.SetActive(false);
-        debugText.SetActive(true);
     }
 
     public void openSettings()
