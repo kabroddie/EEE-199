eab455846c35cbc08df14277c65c26c097c43b2b
yugotrope <jumalonraj55@gmail.com>
2025-05-17 14:22:28 +0800

    Confirmation button for Exit

--- Assets/SettingsEnabler.cs
+++ Assets/SettingsEnabler.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SettingsEnabler : MonoBehaviour
 {
     public GameObject settings;
     public GameObject reminderPanel;
+    public RectTransform confirmExitPanel;
+    
+
+    public Button backButton;
+
+    public float animationTime = 0.3f; // Animation speed
+    public float xOffset = -100f; 
+    public float spacing = 80f;
 
     private void Start()
     {
         reminderPanel.SetActive(true);
+        ResetPositions();
+        backButton.onClick.AddListener(confirmExit);
         
     }
 
@@ -26,5 +38,37 @@ public class SettingsEnabler : MonoBehaviour
     public void closeSettings()
     {
         settings.SetActive(false);
+    }
+
+    private void confirmExit(){
+        Vector2 targetPos = backButton.GetComponent<RectTransform>().anchoredPosition 
+                                + new Vector2(xOffset,-spacing); // Expands downward & shifts left
+
+            confirmExitPanel.DOAnchorPos(targetPos, animationTime)
+                        .SetEase(Ease.OutBack);
+
+            confirmExitPanel.DOScale(1, animationTime).SetEase(Ease.OutBack);
+            confirmExitPanel.GetComponent<CanvasGroup>().DOFade(1, animationTime);
+    }
+
+    public void confirmExitNo(){
+        Vector2 targetPos = backButton.GetComponent<RectTransform>().anchoredPosition + new Vector2(-30f, 0); // Shifted left
+            
+            confirmExitPanel.DOAnchorPos(targetPos, animationTime)
+                .SetEase(Ease.InBack);
+
+            confirmExitPanel.DOScale(0, animationTime).SetEase(Ease.InBack);
+
+            CanvasGroup canvasGroup = confirmExitPanel.GetComponent<CanvasGroup>();
+            if (canvasGroup != null)
+            {
+                canvasGroup.DOFade(0, animationTime); // Fade out
+            }
+    }
+
+    private void ResetPositions(){
+        confirmExitPanel.anchoredPosition = backButton.GetComponent<RectTransform>().anchoredPosition;
+        confirmExitPanel.localScale = Vector3.zero;
+        confirmExitPanel.GetComponent<CanvasGroup>().alpha = 0;
     }
 }
